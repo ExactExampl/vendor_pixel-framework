7f89dede0026d3c810b3e76ba486e3d6bc50b7bc
ExactExampl <64069095+ExactExampl@users.noreply.github.com>
2022-09-18 15:51:08 +0300

    Nuke elmyra
    This reverts commit a88c2af3ae2ec35a14e3b1654331c1b46286a552.

--- SystemUIGoogle/src/com/google/android/systemui/dagger/SystemUIGoogleModule.java
+++ SystemUIGoogle/src/com/google/android/systemui/dagger/SystemUIGoogleModule.java
@@ -26,7 +26,6 @@ import android.os.PowerManager;
 
 import androidx.annotation.Nullable;
 
-import com.android.internal.logging.UiEventLogger;
 import com.android.keyguard.KeyguardViewController;
 import com.android.systemui.assist.AssistManager;
 import com.android.systemui.broadcast.BroadcastDispatcher;
@@ -78,7 +77,6 @@ import com.android.systemui.volume.dagger.VolumeModule;
 import com.google.android.systemui.assist.AssistManagerGoogle;
 import com.google.android.systemui.assist.dagger.AssistModule;
 import com.google.android.systemui.columbus.dagger.ColumbusModule;
-import com.google.android.systemui.elmyra.dagger.ElmyraModule;
 import com.google.android.systemui.dreamliner.DockObserver;
 import com.google.android.systemui.dreamliner.dagger.DreamlinerModule;
 import com.google.android.systemui.power.dagger.PowerModuleGoogle;
@@ -90,15 +88,12 @@ import com.google.android.systemui.smartspace.dagger.SmartspaceModule;
 import com.google.android.systemui.statusbar.dagger.StartCentralSurfacesGoogleModule;
 import com.google.android.systemui.statusbar.KeyguardIndicationControllerGoogle;
 import com.google.android.systemui.statusbar.policy.BatteryControllerImplGoogle;
-import com.google.android.systemui.elmyra.ServiceConfigurationGoogle;
 
 import javax.inject.Named;
 
 import dagger.Binds;
 import dagger.Module;
 import dagger.Provides;
-
-import dagger.Lazy;
 
 @Module(includes = {
         MediaModule.class,
@@ -110,7 +105,6 @@ import dagger.Lazy;
         DreamlinerModule.class,
         ReverseChargingModule.class,
         AssistModule.class,
-        ElmyraModule.class,
         ColumbusModule.class,
 })
 public abstract class SystemUIGoogleModule {
